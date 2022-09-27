9efbb90027341f3bec6759648eeac71212f407b7
unknown <ggndeosi@gmail.com>
2022-09-28 01:55:28 +0530

    + mouse controlled velocity

--- physim/sim.py
+++ physim/sim.py
@@ -22,32 +22,55 @@ def main():
                 self.x_pos = 0
                 self.y_pos = 0
                 self.ini_velocity = ini_velocity
+                self.x = 0
+                self.radius =  19
                 
             def create_particle(self):
-                pygame.draw.circle(screen, (255,255,255),(self.ini_x_pos,self.ini_y_pos),15,1)
+                pygame.draw.circle(screen, (255,255,255),(self.ini_x_pos,self.ini_y_pos),self.radius,1)
             def update_position(self):
                 vx_vector,vy_vector = self.ini_velocity
                 self.ini_x_pos += vx_vector*time_step
                 self.ini_y_pos += vy_vector*time_step
-                pygame.draw.circle(screen, (255,255,255),(self.ini_x_pos,self.ini_y_pos),15,1)
+                pygame.draw.circle(screen, (255,255,255),(self.ini_x_pos,self.ini_y_pos),self.radius,1)
+
                 
         #to store all the particle that are created during program execution
         particle_list = []
+        button_up = False
         while running:
             # clock.tick(120)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
-                if event.type == pygame.MOUSEBUTTONUP:
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    button_up = False
                     x,y = pygame.mouse.get_pos()
-                    particle = Particle(x,y,(10,10))
+                    if not button_up:
+                        pygame.draw.circle(screen,(255,255,255),(x,y),10,6)
+                        print("this ran!")
+                    # particle = Particle(x,y,(-10,0))
+                    # particle.create_particle()
+                    # particle_list.append(particle)
+                if event.type == pygame.MOUSEBUTTONUP:
+                    button_up = True
+                    x1,y1  = pygame.mouse.get_pos()
+                    velocity = (x-x1,y-y1)
+                    particle = Particle(x,y,velocity)
                     particle.create_particle()
                     particle_list.append(particle)
+
             if particle_list:
                 for particle in particle_list:
-                    particle.update_position()
+                    x_constraint = particle.ini_x_pos - WIDTH
+                    y_constraint = particle.ini_y_pos - HEIGHT
+                    
+                    if x_constraint < -(particle.radius+WIDTH) or x_constraint > (particle.radius) or y_constraint > (particle.radius) or y_constraint < -(particle.radius+HEIGHT):
+                        particle_list.remove(particle)
+                    else:
+                        particle.update_position()
+            print(particle_list)
             pygame.display.flip()
-            screen.fill((0,0,0))
+            # screen.fill((0,0,0))
 
 
 if __name__ == '__main__':
